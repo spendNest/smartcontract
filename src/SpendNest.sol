18d4d35c96e45fa64300485df373b9742b915a00
vicsnet <adesanmiadebisi@gmail.com>
2023-10-07 21:06:29 +0100

    docs

--- src/SpendNest.sol
+++ src/SpendNest.sol
@@ -3,7 +3,7 @@
 pragma solidity ^0.8.17;
 
 contract SpendNest{
-        collect payment as sDai
+        // collect payment as sDai
         //Depositing stable_coin ___they can deposit to our contract
         // withdrwaing stable coin
         //token transfer between people having account on the system
