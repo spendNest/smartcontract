087d5e3897003119a7f03931bce4240347aaafa4
vicsnet <adesanmiadebisi@gmail.com>
2023-10-07 21:03:12 +0100

    documentation

---
+++ src/SpendNest.sol
@@ -0,0 +1,16 @@
+// SPDX-License_Identifier:MIT
+
+pragma solidity ^0.8.17;
+
+contract SpendNest{
+        collect payment as sDai
+        //Depositing stable_coin ___they can deposit to our contract
+        // withdrwaing stable coin
+        //token transfer between people having account on the system
+        //Granting someone access to spend fund - whitelist address
+        // Savings club_ ___ deposit to spark protocol 
+                        // __ percentage will be shared 
+        // ##create a goal 
+        //lending & borrowing __ only people that have savings in thesaving club can borrow
+
+}
