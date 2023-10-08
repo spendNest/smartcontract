7ab72cec3411f836e4d57e95bc949b5c0c9d494f
vicsnet <adesanmiadebisi@gmail.com>
2023-10-08 12:48:17 +0100

    function writing

--- src/SpendNest.sol
+++ src/SpendNest.sol
@@ -2,15 +2,71 @@
 
 pragma solidity ^0.8.17;
 
-contract SpendNest{
-        // collect payment as sDai
-        //Depositing stable_coin ___they can deposit to our contract
-        // withdrwaing stable coin
-        //token transfer between people having account on the system
-        //Granting someone access to spend fund - whitelist address
-        // Savings club_ ___ deposit to spark protocol 
-                        // __ percentage will be shared 
-        // ##create a goal 
-        //lending & borrowing __ only people that have savings in thesaving club can borrow
-        //Comet,Polygon, Tableland and Compound.
+contract SpendNest {
+        struct userAccount{
+                uint256 totalSavings;
+                uint256 availableBalance;
+                uint256 sharedBalance;
+                uint256 clubSavings;
+        }
+    mapping (address => userAccount) myAccount;
+    
+    address TokenAccepted;
+
+    // collect payment as sDai
+
+    // EVENTS
+
+      //Depositing stable_coin ___they can deposit to our contract
+    /**
+Deposit function
+ */
+    function depositFund(uint _amount) external {
+
+    }
+
+/**
+* View my fund
+*/ 
+function viewAccount() external returns(userAccount memory){
+
+}
+ // withdrwaing stable coin
+/**
+*Withdraw Fund
+ */
+    function withdrawFund() external{
+
+    }
+
+/**
+*Total savings
+*/
+  
+   
+    //token transfer between people having account on the system
+        /**
+        * Transfer within address registered on the contract
+        */
+        function transferFund() external{
+
+        }
+
+
+    //Granting someone access to spend fund - whitelist address
+
+/**
+* Grant users Access to spend your fund
+*/     
+function grantAccessToFund() external{
+
+}
+
+    // Savings club_ ___ deposit to spark protocol
+
+
+    // __ percentage will be shared
+    // ##create a goal
+    //lending & borrowing __ only people that have savings in thesaving club can borrow
+    //Comet,Polygon, Tableland and Compound.
 }
