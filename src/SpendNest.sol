54117d5cd4597031a86545a7e7a39503756d9a92
dunnie99 <oluwadunnirachael@gmail.com>
2023-10-08 15:01:22 +0100

    function deposit fund done

--- src/SpendNest.sol
+++ src/SpendNest.sol
@@ -1,28 +1,70 @@
 // SPDX-License_Identifier:MIT
-
-pragma solidity ^0.8.17;
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+pragma solidity ^0.8.20;
 
 contract SpendNest {
         struct userAccount{
                 uint256 totalSavings;
-                uint256 availableBalance;
+                uint256 availableBalance;  
                 uint256 sharedBalance;
-                uint256 clubSavings;
+                uint256 noOfClubs;
         }
+        struct clubCreated{
+            string clubName;
+            uint startDate;
+            uint endDate;
+            uint savingsGoal;
+            uint totalParticipant;
+            bool aUser;
+            
+        }
+        mapping(address =>bool) accountCreated;
     mapping (address => userAccount) myAccount;
     
     address TokenAccepted;
-
+    address owner = msg.sender;
+    
     // collect payment as sDai
 
     // EVENTS
+    event Transfer(
+        address indexed user,
+        uint256 indexed amount,
+        uint256 indexed time
+    );
+
+    function set_Token(address _tokenAceppted) external{
+        require(msg.sender == owner, "Not Owner");
+        TokenAccepted = _tokenAceppted;
+    }
+
+    function createAccount() external {
+        address user = msg.sender;
+        require(accountCreated[user] == false, 'ACCOUNT_ALREADY_EXIST');
+        accountCreated[user] = true;
+        userAccount memory newUser = userAccount({
+            totalSavings: 0,
+            availableBalance: 0,
+            sharedBalance: 0 ,
+            noOfClubs: 0
+        });
+
+        //  Person memory newPerson = Person(_name, _age);
+        myAccount[user] = newUser;
+    }
 
       //Depositing stable_coin ___they can deposit to our contract
     /**
 Deposit function
  */
     function depositFund(uint _amount) external {
-
+        address user = msg.sender;
+        uint256 senderBal = IERC20(TokenAccepted).balanceOf(user);
+        require(senderBal >= _amount, "senderBal not sufficient");
+        require(IERC20(TokenAccepted).transferFrom(user, address(this), _amount), "transfer Failed");
+        userAccount storage _user = myAccount[user];
+        _user.availableBalance += _amount;
+        emit Transfer(user, _amount, block.timestamp);
     }
 
 /**
