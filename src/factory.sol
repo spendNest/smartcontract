582c06132e477865da6265f72d05fe27569f6c15
dunnie99 <oluwadunnirachael@gmail.com>
2023-10-15 20:32:24 +0100

    mir

--- src/factory.sol
+++ src/factory.sol
@@ -1,61 +1,210 @@
 // SPDX-License_Identifier:MIT
 pragma solidity ^0.8.21;
-
+import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "./interface/ICompound.sol";
 import "./SpendNest.sol";
 
 contract factory {
- address[] childContracts;
- address tokenAccepted;
+    address[] childContracts;
+    address tokenAccepted;
+    address compound;
+    address owner = msg.sender;
 
- mapping(address => address) myAddress;
-
-
-
-
-function set_Token(address _tokenAceppted) external {
-        //require(msg.sender == owner, "Not Owner");
-        tokenAccepted = _tokenAceppted;
+    struct ClubCreated {
+        string clubName;
+        uint startDate;
+        uint endDate;
+        uint savingsGoal;
+        uint totalParticipant;
+        mapping(address => bool) aUser;
+        mapping(address => uint256) myBalance;
     }
-function createAcount () external {
-    SpendNest newContract = new SpendNest(address(this));
-    childContracts.push(address(newContract));
-    myAddress[msg.sender] = address(newContract);
-}
+    string[] publicClubsNames;
 
-function _returnAddress(address _contractOwner) external view returns(address) {
-    
- address contractAddress = myAddress[_contractOwner];
-    return contractAddress;
-}
+    mapping(address => address) myAddress;
+    mapping(address => bool) userExists;
+    mapping(string => ClubCreated) publicClubs;
 
-function _tokenAddress () public view returns(address) {
-    return tokenAccepted;
-}
+    function set_Token(address _tokenAcepted) external {
+        require(msg.sender == owner, "Not Owner");
+        tokenAccepted = _tokenAcepted;
+    }
 
-functino
+    function setCompound(address _compound) external {
+        require(msg.sender == owner, "Not Owner");
+        compound = _compound;
+    }
+
+    function createAcount() external {
+        SpendNest newContract = new SpendNest(address(this));
+        childContracts.push(address(newContract));
+        myAddress[msg.sender] = address(newContract);
+        userExists[msg.sender] = true;
+    }
+
+    function _returnAddress(
+        address _contractOwner
+    ) external view returns (address) {
+        address contractAddress = myAddress[_contractOwner];
+        return contractAddress;
+    }
+
+    function _tokenAddress() public view returns (address) {
+        return tokenAccepted;
+    }
+
+    function userExist(address _user) external view returns (bool) {
+        return userExists[_user];
+    }
+
+    /**
+     * check if the name already exist
+     */
+    function checkNameExist(string memory _name) internal view returns (bool) {
+        string[] memory allNames = publicClubsNames;
+        if (allNames.length > 0) {
+            for (uint256 index = 0; index < allNames.length; index++) {
+                if (
+                    keccak256(bytes(_name)) == keccak256(bytes(allNames[index]))
+                ) {
+                    revert("exists");
+                }
+                break;
+            }
+        }
+        return true;
+    }
+
+    /**
+     *create public clubs
+     */
+    function createPublicClubs(
+        string memory _clubName,
+        uint256 _startDate,
+        uint256 _endDate,
+        uint256 _savingsGoal
+    ) external {
+        address _user = msg.sender;
+        require(userExists[_user], "ACCOUNT_DOES_NOT_EXIST");
+        require(checkNameExist(_clubName) == false, "CLUB_NAME_EXIST");
+        ClubCreated storage createClub = publicClubs[_clubName];
+        createClub.clubName = _clubName;
+        createClub.startDate = _startDate;
+        createClub.endDate = _endDate;
+        createClub.savingsGoal = _savingsGoal;
+        createClub.totalParticipant += 1;
+        createClub.aUser[_user] = true;
+    }
+
+    /**
+     * join savings club
+     */
+    // ##create a goal
+    // __ percentage will be shared
+
+    function joinSavingsClub(string memory _clubName) external {
+        address _user = msg.sender;
+        require(userExists[_user], "ACCOUNT_DOES_NOT_EXIST");
+        require(checkNameExist(_clubName), "CLUB_DOES_NOT_EXIST");
+        ClubCreated storage createClub = publicClubs[_clubName];
+        createClub.aUser[_user] = true;
+        createClub.totalParticipant += 1;
+    }
+
+    /**
+     * move fund to savings club
+     */
+    function addFundSavingsClub(
+        string memory _clubName,
+        uint256 _amount
+    ) external {
+        address _user = msg.sender;
+        address token = tokenAccepted;
+        address _compound = compound;
+        require(userExists[_user], "ACCOUNT_DOES_NOT_EXIST");
+        require(checkNameExist(_clubName), "CLUB_DOES_NOT_EXIST");
+        ClubCreated storage createClub = publicClubs[_clubName];
+        //userAccount storage _myOwnAccount = myAccount[_user];
+        uint _balance = IERC20(token).balanceOf(_user);
+
+        require(
+            block.timestamp >= createClub.startDate,
+            "SAVINGS_CLUB_NOT_STARTED"
+        );
+        require(_balance >= _amount, "INSUFFICIENT_BALANCE");
+        require(block.timestamp <= createClub.endDate, "SAVINGS_ENDED");
+        require(createClub.aUser[_user], "NOT_A_USER");
+        //IERC20(token).approve(_compound, _amount);
+        ICompound(_compound).supply(token, _amount);
+        createClub.myBalance[_user] += _amount;
+        //_myOwnAccount.totalSavings += _amount;
+
+        //emit PublicClubDeposit(_user, _amount, block.timestamp);
+    }
+
+    function showPublicData()
+        external
+        view
+        returns (
+            string[] memory,
+            uint[] memory,
+            uint[] memory,
+            uint[] memory,
+            uint[] memory
+        )
+    {
+        uint length = publicClubsNames.length;
+        string[] memory clubName = new string[](length);
+        uint[] memory startDate = new uint[](length);
+        uint[] memory endDate = new uint[](length);
+        uint[] memory savingsGoal = new uint[](length);
+        uint[] memory totalParticipant = new uint[](length);
+        for (uint256 i = 0; i < length; i++) {
+            string memory currentClubName = publicClubsNames[i];
+            ClubCreated storage currentClub = publicClubs[currentClubName];
+
+            clubName[i] = currentClub.clubName;
+            startDate[i] = currentClub.startDate;
+            endDate[i] = currentClub.endDate;
+            savingsGoal[i] = currentClub.savingsGoal;
+            totalParticipant[i] = currentClub.totalParticipant;
+        }
+        return (clubName, startDate, endDate, savingsGoal, totalParticipant);
+    }
 
 
+    function showMyPublicSavings(
+        string memory _clubName
+    ) external view returns (uint256) {
+        ClubCreated storage createClub = publicClubs[_clubName];
+        return createClub.myBalance[msg.sender];
+    }
 
+    /**
+     *remove fund from saving
+     */
+    function withdrawPublicClub(string memory _clubName) external {
+        address _user = msg.sender;
+        address _compound = compound;
+        require(userExists[_user], "ACCOUNT_DOES_NOT_EXIST");
+        require(checkNameExist(_clubName), "CLUB_DOES_NOT_EXIST");
+        ClubCreated storage createClub = publicClubs[_clubName];
+        uint256 borrowBal = ICompound(_compound).borrowBalanceOf(_user);
+        uint256 amount = createClub.myBalance[_user];
+        if (borrowBal == 0 ) {
+            ICompound(_compound).withdraw(tokenAccepted, amount);
+            createClub.myBalance[_user] = 0;
+        }  else if(borrowBal > 0 && amount > borrowBal){
+           uint256 rem = amount - borrowBal;
+           ICompound(_compound).withdraw(tokenAccepted, rem);
+           createClub.myBalance[_user] = 0;
 
+        } else {
+            revert("YOU_HAVE_UNPAID_OVERDRAFT");
+        }
+        
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 
 
 
