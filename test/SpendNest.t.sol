e40ca8d8463f257b9dbacff098ee74ff3e7b1bce
vicsnet <adesanmiadebisi@gmail.com>
2023-10-17 12:18:30 +0100

    Test started

---
+++ test/SpendNest.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/SpendNest.sol";
+import "../src/factory.sol";
+
+contract SpendNestTest is Test{
+        
+}
