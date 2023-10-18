ad037a5d7f0518936f288944027acdd846accfd8
vicsnet <adesanmiadebisi@gmail.com>
2023-10-18 11:47:17 +0100

    read me added

---
+++ readme.md
@@ -0,0 +1,2 @@
+##Factory contract
+https://mumbai.polygonscan.com/address/0xa8d1b6fab28a0b653befbdd3b1f8fe6ea36f78f5#writeContract
--- src/SpendNest.sol
+++ src/SpendNest.sol
@@ -2,51 +2,10 @@
 import "../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ICompound} from "./interface/ICompound.sol";
 import "./factory.sol";
-pragma solidity ^0.8.20;
+import "./interface/Ifactory.sol";
+pragma solidity ^0.8.21;
 
-interface Ifactory {
-    function createAcount() external;
 
-    function _returnAddress(
-        address _contractOwner
-    ) external view returns (address);
-
-    function tokenAddress() external view returns (address);
-
-    function userExist(address _user) external view returns (bool);
-
-    function createPublicClubs(
-        string memory _clubName,
-        uint256 _startDate,
-        uint256 _endDate,
-        uint256 _savingsGoal
-    ) external;
-
-    function joinSavingsClub(string memory _clubName) external;
-
-    function addFundSavingsClub(
-        string memory _clubName,
-        uint256 _amount,
-        address _user
-    ) external;
-
-    function showPublicData()
-        external
-        view
-        returns (
-            string[] memory,
-            uint[] memory,
-            uint[] memory,
-            uint[] memory,
-            uint[] memory
-        );
-
-    function showMyPublicSavings(
-        string memory _clubName
-    ) external view returns (uint);
-
-    function withdrawPublicClub(string memory _clubName, address _user) external returns(uint256, uint256);
-}
 
 contract SpendNest {
     // how do we have access to money been shared
